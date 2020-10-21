75ff97bd2322d8ded861e924b9e00b2c855758d8
ilkerkurtulan97 <45030821+ilkerkurtulan97@users.noreply.github.com>
2020-10-21 20:09:45 +0300

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3 class="alert alert-danger">{{msg}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,108 @@
+<template>
+    <div>
+      <p v-if="products.length===0">Currently There are no products . . . </p>
+      <table class="table">
+        <thead>
+          <tr>
+
+            <th>Id</th>
+            <th>Product Name</th>
+            <th>Category</th>
+            <th>Explanation</th>
+            <th>Product price</th>
+            <th>Stock number</th>
+            <th></th>
+
+          </tr>
+        </thead>
+
+        <tbody>
+
+
+          <tr v-for="product in products" :key="product.id">
+            <td v-if="updateId===product.id">
+              <input v-model="product.productName" type="text" class="form-control" id="id">
+            </td>
+            <td v-else>
+              {{ product.id}}
+            </td>
+            <td v-if="updateId===product.id">
+              <input v-model="product.productName" type="text" class="form-control" id="productName">
+            </td>
+            <td v-else>
+              {{ product.productName}}
+            </td>
+            <td v-if="updateId===product.id">
+              <input v-model="product.categoryId" type="text" class="form-control" id="categoryId">
+            </td>
+            <td v-else>
+              {{ product.categoryId}}
+            </td>
+            <td v-if="updateId===product.id">
+              <input v-model="product.quantityPerUnit" type="text" class="form-control" id="quantityPerUnit">
+            </td>
+            <td v-else>
+              {{ product.quantityPerUnit}}
+            </td>
+            <td v-if="updateId===product.id">
+              <input v-model="product.unitPrice" type="text" class="form-control" id="unitPrice">
+            </td>
+            <td v-else>
+              {{ product.unitPrice}}
+            </td>
+            <td v-if="updateId===product.id">
+              <input v-model="product.unitsInStock" type="text" class="form-control" id="unitsInStock">
+            </td>
+            <td v-else>
+              {{ product.unitsInStock }}
+            </td>
+            
+            <td v-if="updateId!==product.id">
+              <button class="btn btn-sm btn-primary" @click="handleUpdate(product)">Update</button>
+              <button class="btn btn-sm btn-danger" @click="handleDelete(product)">Delete</button>
+            </td>
+
+            <td v-else>
+              <button class="btn btn-sm btn-primary" @click="handleUpdate(product)">Save</button>
+              <button class="btn btn-sm btn-danger" @click="handleDelete(product)">Cancel</button>
+            </td>
+
+          </tr>
+        </tbody>
+
+      </table>
+      
+
+
+    </div>
+</template>
+
+<script>
+export default {
+  name:"product-list",
+  data(){
+    return { updateId:null };
+  },
+  props:{
+    products:Array
+  },
+  methods: {
+    handleDelete(product) {
+      this.$emit("delete:product", product);
+    },
+    handleUpdate(product) {
+      this.updateId = product.id
+    },
+    handleSave(product) {
+      this.$emit("update:product",product)
+      this.updateId = null
+    }
+  }
+};
+</script>
+
+
+
+<style scoped>
+
+</style>
